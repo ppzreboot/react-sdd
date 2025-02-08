260c597b7ec6badd6fbe9689373ebb729d8976ed
PPz <ppzreboot@qq.com>
2025-02-08 15:30:59 +0800

    feat: dmeo

--- demo/app.tsx
+++ demo/app.tsx
@@ -7,7 +7,32 @@ function APP() {
 }
 
 function Page2(props: I_page2) {
+  const user = props.user.useState()
   return (
-    <div>test</div>
+    <div>
+      <p>User's name is {user.name}. User's age is {user.age}.</p>
+      <div>
+        <label>Name</label>
+        <input
+          value={user.name}
+          onChange={evt => {
+            props.user.set_state(os => ({
+              name: evt.target.value,
+            }))
+          }}
+        />
+      </div>
+      <div>
+        <label>Age</label>
+        <input
+          value={user.age}
+          onChange={evt => {
+            props.user.set_state(os => ({
+              age: Number(evt.target.value),
+            }))
+          }}
+        />
+      </div>
+    </div>
   )
 }
